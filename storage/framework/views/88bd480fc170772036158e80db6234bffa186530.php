3ac760087537b71ef35731b81f5b9bfdbc50952c
farah <farahramzy123@gmail.com>
2023-05-07 22:15:48 +0300

    edits

---
+++ storage/framework/views/88bd480fc170772036158e80db6234bffa186530.php
@@ -0,0 +1,40 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Convert Website Images to WebP</title>
+</head>
+<body>
+    <h1>Convert Website Images to WebP</h1>
+
+    <?php if($errors->any()): ?>
+        <div>
+            <ul>
+                <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li><?php echo e($error); ?></li>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+
+    <form method="POST" action="<?php echo e(route('convert')); ?>">
+        <?php echo csrf_field(); ?>
+        <div>
+            <label for="website_url">Website URL:</label>
+            <input type="url" name="website_url" required>
+        </div>
+        <button type="submit">Convert</button>
+    </form>
+</body>
+</html> 
+
+
+
+
+
+
+
+<?php /**PATH F:\last semester\final project\seopro\resources\views/website_image.blade.php ENDPATH**/ ?>
