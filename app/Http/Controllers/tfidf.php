1360883b1bff44e7ac57d90407d9a9207ab04bee
hashimsaid <hashim_becool@hotmail.com>
2023-05-12 19:08:10 +0300

    comments added to the tfidf controller

--- app/Http/Controllers/tfidf.php
+++ app/Http/Controllers/tfidf.php
@@ -9,7 +9,7 @@ class tfidf extends Controller
     
     public function index(){
    
-
+// redirect into the flask webserver api
         return redirect('http://127.0.0.1:4000/tfidf');
         
         
