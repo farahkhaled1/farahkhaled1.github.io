5b7e9c6571dccac7e0aa55c6fad2292b22e10db9
hashimsaid <hashim_becool@hotmail.com>
2023-05-12 19:07:26 +0300

    tfidf controller edited

--- app/Http/Controllers/tfidf.php
+++ app/Http/Controllers/tfidf.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,11 +8,11 @@ class tfidf extends Controller
 {
     
     public function index(){
+   
 
-        
         return redirect('http://127.0.0.1:4000/tfidf');
         
-        // return view('tfidf');
+        
     }
 
 
